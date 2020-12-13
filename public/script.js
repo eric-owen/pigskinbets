d8c3c07157b272eeab748be69e209bd4e58d40aa
eric-owen <65836532+eric-owen@users.noreply.github.com>
2020-12-13 12:59:43 -0500

    render data from api to the bets page. also adjusted styling and added a bet slip

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 const API_Key = config.API_Key
 
-async function getScores (week) {
+async function getScores(week) {
     const response = await axios.get(`https://charlotte.rotogrinders.com/sports/nfl/events?key=${API_Key}&week=2020-reg-${week}`);
     return await response.data.data;
 };
@@ -11,7 +11,47 @@ document.addEventListener('DOMContentLoaded', () => {
         e.preventDefault();
         let week = document.getElementById('week-selector');
         let weekValue = week.value;
-        let games = await getScores(weekValue)
-        console.log(games);
+        let games = await getScores(weekValue);
+
+        const betClass = document.querySelector('.bets-list')
+        betClass.innerHTML = ''
+
+        try {
+            games.forEach(game => {
+                const createDiv = document.createElement('div')
+                createDiv.classList.add('indiv-bet')
+                createDiv.innerHTML = `
+                
+                <div class="col">
+                        <p>12/10/2020</p>
+                        <p>1:00 PM</p>
+                    </div>
+                <div class="col">
+                    <p>${game.away.city} ${game.away.mascot}</p>
+                    <p>${game.home.city} ${game.home.mascot}</p>
+                </div>
+                <div class="col">
+                    <p>${game.odds.current.spread.away}</p>
+                    <p>${game.odds.current.spread.home}</p>
+                </div>
+                <div class="col">
+                    <p>${game.odds.current.moneyline.away}</p>
+                    <p>${game.odds.current.moneyline.home}</p>
+                </div>
+                <div class="col">
+                    <p>O${game.odds.current.total.value} ${game.odds.current.total.over}</p>
+                    <p>U${game.odds.current.total.value} ${game.odds.current.total.under}</p>
+                </div>
+            </div>
+            `
+                betClass.appendChild(createDiv)
+                console.log(game)
+
+            })
+        } catch {
+            betClass.innerHTML = 'No Odds generated'
+        }
+
+
     });
 });
