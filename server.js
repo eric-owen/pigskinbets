cd2ebdec220e92f8b2fff4accf410410cf9f81f3
Nicholas Ochs <nick.ochs@yahoo.com>
2020-12-13 17:15:32 -0500

    adds Google OAuth setup, should refactor before merging

--- server.js
+++ server.js
@@ -1,15 +1,63 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const PORT = 3000;
 
+const betRoutes = require('./routes/bets');
+const performanceRoutes = require('./routes/performance');
 
-const betRoutes = require('./routes/bets')
-const performanceRoutes = require('./routes/performance')
 
-//OAuth Configuration
-const OAuth = require('oauth');
-const session = require('express-session');
+//Passport-GoogleOAuth20 Configuration
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const cookieSession = require('cookie-session');
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(cookieSession({
+    name: 'pigskinbets-session',
+    keys: ['key1', 'key2']
+}));
+
+// const isLoggedIn = (req, res, next) => {
+//     if (reg.user) {
+//         next();
+//     } else {
+//         res.status(401);
+//     };
+// }; pass isLoggedIn function into the bets route before (req, res)
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(err, user);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.callbackURL
+},
+function(accessToken, refreshToken, profile, done) {
+    // use the profile info (mainly profile id) to check if the user is registered in our DB
+    // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return done(null, profile); //or return done(err, user)
+    // });
+}
+));
+
+app.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/google/callback', passport.authenticate('google', { failureRedirect: '/bets' }),
+function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/bets');
+});
+
+//Static Files
 app.use(express.static('public'))
 
 //Middleware
