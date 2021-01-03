038ffae0ee719a5476b6664e28236cf535064323
Joseph Epherson <josephepherson@Josephs-MacBook-Air.local>
2021-01-03 17:22:51 -0500

    fully connected

--- routes/bets.js
+++ routes/bets.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
 
 const controller = require('../controllers/bets');
+
 
 //Equivalent to app.get('/bets')
 router.get('/', controller.games); //will display all games that week
 
-router.post('/', controller.placeBets); //will submit users bets
+router.post('/', controller.placeBets)//will submit users bets
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,9 +3,63 @@ const app = express();
 require('dotenv').config();
 const PORT = 3000;
 
+app.use(express.urlencoded());
+app.use(express.json());
+
 const homeRoutes = require('./routes/home');
 const betRoutes = require('./routes/bets');
 const performanceRoutes = require('./routes/performance');
+
+// // MongoDB database connection
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+mongoose.connect("mongodb+srv://new-user-13:houston13@pigskinbets.ifnrc.mongodb.net/Bets?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+});
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+    console.log("DB connected for real...")
+});
+const {Schema} = mongoose
+const betSchema = new Schema({
+    userName: String,
+    betType:  String,
+    betAmount: Number,
+    betTeam: String,
+    homeTeam: String,
+    awayTeam: String,
+    odds: Number,
+    risk: Number,
+    gameStatus: String,
+    potentialWinnings: Number,
+    overUnder: String,
+    open: Boolean,
+    betstatus: String,
+    date: { type: Date, default: Date.now },
+    hidden: Boolean,
+    meta: {
+        winings: Number,
+        total:  Number
+    }
+});
+
+const userSchema = new Schema({
+    name: String,
+    userName: String,
+    currentBankRoll: Number,
+    history: {
+        wins: Number,
+        losses: Number,
+        totalEarnings: Number,
+    },
+});
+let Bet = mongoose.model('Bet', betSchema);
+const User = mongoose.model('User', userSchema);
+
 
 
 //Passport-GoogleOAuth20 Configuration
@@ -52,6 +106,112 @@ app.get('/google/callback',
     res.redirect('/');
 });
 
+app.get('/findSomeBetRoute', (req,res) => {
+    let findBetsByName = (usersNames,done) => {
+        Bet.find({userName: usersNames}, (err, betResults) => {
+            if(err){
+                console.log(err);
+            }else{
+                console.log(betResults)
+            }
+        });
+    }
+    findBetsByName('Big Money Mike');
+    res.status(200).json({
+        message: 'These are your current bets',
+        body: JSON.stringify(res.data)
+    })
+    console.log(req.body);
+})
+
+app.get('/findSomeUserRoute', (req,res) => {
+    User.findOne({userName: 'Big Money Mike'}, (err, data) => {
+        if(err) {
+            console.log(err)
+        }else{
+            console.log(data)
+        }
+    })
+    res.status(200).json({
+        message: `Hello Mike`
+    })
+});
+
+app.post('/someBetsRoute', (req , res) => {
+    // Adding info into database
+    const newBet= new Bet({
+        userName: 'Big Money Mike',
+        betType: req.body.data.betType,
+        betAmount: req.body.data.risk,
+        homeTeam: req.body.data.homeTeam,
+        awayTeam: req.body.data.awayTeam,
+        gameStatus: req.body.data.gameStatus,
+        betTeam: req.body.data.betTeam,
+        odds: req.body.data.betsOdds,
+        potentialWinnings: req.body.data.potentialWinnings,
+        risk: req.body.data.betRisk,
+        open: true,
+        betStatus: 'pending'
+    
+    });
+        newBet.save(function(err) {
+            if (err) return handleError(err);
+        });
+    res.status(200).json({
+        message: "Bet placed successfully, GL",
+        body: JSON.stringify(res.data)
+    });
+})
+
+app.post('/someUserRoute', (req , res) => {
+    // Adding info into database
+    const newUser= new Users({
+        name: 'MIke',
+        userName: 'Big Money Mike',
+        currentBankRoll: 10000,
+        history: {
+            wins: 20,
+            losses: 5,
+            totalEarnings: 30000,
+        },
+    });
+        newUser.save(function(err) {
+            if (err) return handleError(err);
+        });
+
+    res.status(200).json({
+        message: `Hello Mike, good luck out there!`
+    });
+});
+
+app.delete('/someDeleteRoute', (req,res) => {
+    Bet.findOneAndRemove({ betTeam: 'Saints'}, (err, data) => {
+        if(err) {
+            return handleError(err)
+        }else {
+            console.log(data)
+        }
+    });
+    res.status(200).json({
+        message: 'You were takin out of this bet'
+    })
+})
+
+app.patch('/someUpdateRoute', (req,res) => {
+        Bet.findOneAndUpdate({betTeam: 'Cowboys'}, { betTeam: 'Saints'}, { new: true }, (err, data) => {
+            if(err){
+                console.log(err);
+            }else{
+                console.log(data)
+                data.save()
+            }
+        }); 
+    res.status(200).json({
+        message: 'Bet updated Thank You and Good Luck',
+        body: res.data
+    });
+})
+
 //Static Files
 app.use(express.static('public'))
 
@@ -80,3 +240,16 @@ app.get('/logout', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
+
+
+// (function poll(){
+// setTimeout(function(){
+//     async function getScores(week) {
+//             const response = await axios.get(`https://charlotte.rotogrinders.com/sports/nfl/events?key=${API_Key}&week=2020-reg-${week}`);
+//             return await response.data.data;
+//         };
+//         //Setup the next poll recursively
+//         poll();
+//     },30000);
+// })();
+
