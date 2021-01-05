7e63285f804a816b306a81dcaabe1d0ba5051850
Nicholas Ochs <nick.ochs@yahoo.com>
2021-01-05 14:23:18 -0500

    heroku redeploy5

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ function(accessToken, refreshToken, profile, done) {
 
 app.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-app.get('sleepy-harbor-60617.herokuapp.com/google/callback', 
+app.get('/google/callback', //change this back to /google/callback
     passport.authenticate('google', { failureRedirect: '/' }),
     function(req, res) {
     // Successful authentication, redirect home.
