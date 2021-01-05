912c8a2c5edc235c4d63a18220792aee672851ac
Nicholas Ochs <nick.ochs@yahoo.com>
2021-01-04 21:44:26 -0500

    heroku redeploy3

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const homeRoutes = require('./routes/home');
 const betRoutes = require('./routes/bets');
