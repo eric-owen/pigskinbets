0d7c85592c79e7fd3108e082ebeae4f27ff26f83
Nicholas Ochs <nick.ochs@yahoo.com>
2021-01-05 15:31:42 -0500

    heroku CSP fix attempt8

--- public/bets-script.js
+++ public/bets-script.js
@@ -1,7 +1,7 @@
 // const API_Key = config.API_Key
 
 async function getScores(week) {
-    const response = await axios.get(`https://charlotte.rotogrinders.com/sports/nfl/events?key=${API_Key}&week=2020-${week}`);
+    const response = await axios.get(`https://charlotte.rotogrinders.com/sports/nfl/events?key=ZV03bez9ie16w553FeuM8Z49Djw67pt6&week=2020-${week}`);
     return await response.data.data;
 };
 
